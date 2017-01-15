7d80c814e0f94b0122184fcb88b04687e5e3de5c
Joel Chao <joel.chao@appier.com>
2017-01-15 22:41:07 +0800

    cross validation test

--- feat_baseline.py
+++ feat_baseline.py
@@ -16,14 +16,17 @@ def extract_feature(train_data_file, feature_file):
         pickle.dump(obj=(features, y_train), file=f)
 
 
-def train_linear(feature_file):
+def train_linear(feature_file, n_fold=5, nb_epoch=10):
     with open(feature_file, 'rb') as f:
         x_all, y_all = pickle.load(f)
 
-    kf = KFold(len(y_all), n_folds=5, random_state=791211)
+    models = []
+    kf = KFold(len(y_all), n_folds=n_fold, random_state=791211)
     for train_index, val_index in kf:
         model = linear_model(input_size=x_all.shape[1])
         model.compile(optimizer=Adam(0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         x_train, x_val = x_all[train_index], x_all[val_index]
         y_train, y_val = y_all[train_index], y_all[val_index]
-        model.fit(x_train, y_train, batch_size=50, nb_epoch=5, validation_data=(x_val, y_val))
+        model.fit(x_train, y_train, batch_size=50, nb_epoch=nb_epoch, validation_data=(x_val, y_val))
+        models.append(model)
+    return models
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from keras.models import load_model
 
 from data import load_data
@@ -11,5 +12,19 @@ def testing(test_file, ckpt_file, result_file):
     with open(result_file, 'w') as f:
         print('image,ALB,BET,DOL,LAG,NoF,OTHER,SHARK,YFT', file=f)
         for name, prob in zip(names, probs):
+            prob_str = ','.join('{:.8f}'.format(p) for p in prob)
+            print('{},{}'.format(name, prob_str), file=f)
+
+
+def testing_by_models(test_file, models, result_file):
+    probs = []
+    for model in models:
+        x_test, names = load_data(test_file)
+        prob = model.predict(x_test, batch_size=100, verbose=True)
+
+    avg_prob = np.mean(np.stack(probs))
+    with open(result_file, 'w') as f:
+        print('image,ALB,BET,DOL,LAG,NoF,OTHER,SHARK,YFT', file=f)
+        for name, prob in zip(names, avg_prob):
             prob_str = ','.join('{:.8f}'.format(p) for p in prob)
             print('{},{}'.format(name, prob_str), file=f)
