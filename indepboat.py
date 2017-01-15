ba33a57a0abfe956a573bc4c5355507bbb1fd5e6
Joel Chao <joel.chao@appier.com>
2017-01-15 20:23:33 +0800

    fix signature

--- indepboat.py
+++ indepboat.py
@@ -5,19 +5,13 @@ from model.inception_v3_fc import *
 from boat_data import BoatTrainData
 
 
-def train_inceptionv3_baseline(train_data_file, val_data_file, ckpt_file, hidden_size):
-    print('Building model ...')
-    model = inception_v3_single_fc_model(hidden_size)
-    _train(model, train_data_file, val_data_file, ckpt_file)
-
-
-def train_inceptionv3_mlp(train_data_file, val_data_file, ckpt_file, hidden_sizes, drop_rate=0.2):
+def train_inceptionv3_mlp(train_data_file, ckpt_file, hidden_sizes, drop_rate=0.2):
     print('Building model ...')
     model = inception_v3_muliple_fc_drop_model(hidden_sizes, drop_rate)
-    _train(model, train_data_file, val_data_file, ckpt_file)
+    _train(model, train_data_file, ckpt_file)
 
 
-def _train(model, train_data_file, val_data_file, ckpt_file):
+def _train(model, train_data_file, ckpt_file):
     train_data = BoatTrainData(train_data_file)
     train_datagen, x_val, y_val = train_data.get_train_val_set(seed=0, batch_size=50)
     ckbt_callback = ModelCheckpoint(ckpt_file)
