cad7b803c3ffbed9c4b6c6c4303403330fd18910
Joel Chao <joel.chao@appier.com>
2017-01-15 23:05:11 +0800

    fix label error

--- feat_baseline.py
+++ feat_baseline.py
@@ -2,6 +2,7 @@ import pickle
 
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
+from keras.utils.np_utils import to_categorical
 from sklearn.cross_validation import KFold
 
 from model.inception_v3_feat import *
@@ -19,12 +20,13 @@ def extract_feature(train_data_file, feature_file):
 def train_linear(feature_file, n_fold=5, nb_epoch=10):
     with open(feature_file, 'rb') as f:
         x_all, y_all = pickle.load(f)
+        y_all = to_categorical(y_all, nb_classes=NUM_CLASS)
 
     models = []
     kf = KFold(len(y_all), n_folds=n_fold, random_state=791211)
     for train_index, val_index in kf:
         model = linear_model(input_size=x_all.shape[1])
-        model.compile(optimizer=Adam(0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer=Adam(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
         x_train, x_val = x_all[train_index], x_all[val_index]
         y_train, y_val = y_all[train_index], y_all[val_index]
         model.fit(x_train, y_train, batch_size=50, nb_epoch=nb_epoch, validation_data=(x_val, y_val))
